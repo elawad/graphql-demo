72a4ee58584cfedd5f521299c1b9746ba368da25
Aymen Elawad <aymenz@gmail.com>
2017-11-05 17:08:49 -0500

    client subscriptions

--- client/src/Apollo/index.jsx
+++ client/src/Apollo/index.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
 
 import App from '../App/App';
 
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/subscriptions',
+  options: { reconnect: true }
+});
+
+// Split based on operation type:
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  link,
   cache: new InMemoryCache(),
 });
 
