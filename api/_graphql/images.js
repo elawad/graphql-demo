0f098fe0079f1895a10b16d6d911b67d90582f00
Aymen Elawad <elawad@users.noreply.github.com>
2023-05-21 22:39:59 -0400

    images

--- api/_graphql/images.js
+++ api/_graphql/images.js
@@ -3,17 +3,16 @@ import { Authors, Collections, Sizes } from './data.js';
 
 const IMG_API = 'https://source.unsplash.com/collection';
 const IMAGES = [];
-const COUNT = 12;
 
-// query
-const images = () => (IMAGES.length >= COUNT ? IMAGES : init);
+// Query
+const images = () => init;
 const imageById = (id) => IMAGES.find((i) => i.id === id);
 const imagesByAuthor = (id) => IMAGES.filter((i) => i.authorId === id);
 
 const authors = () => Authors;
 const authorById = (id) => Authors.find((a) => a.id === id);
 
-// mutation
+// Mutation
 function imageVote(id, vote = +1) {
   const image = imageById(id);
   image.likes = Math.max(0, image.likes + vote);
@@ -23,8 +22,8 @@ function imageVote(id, vote = +1) {
   return image;
 }
 
-async function imageCreate(name, i) {
-  const id = (i ?? IMAGES.length) + 1;
+async function imageCreate(name) {
+  const id = getId();
   const data = getData(id, name);
   const urls = await fetchUrl(id).catch(console.log);
   if (!urls) return;
@@ -34,7 +33,7 @@ async function imageCreate(name, i) {
   return image;
 }
 
-// setup
+// Setup
 const getData = (id, name) => ({
   id,
   name: name || `Image ${id}`,
@@ -59,14 +58,18 @@ async function fetchUrl(id) {
   return { urlSm, urlMd };
 }
 
-const wait = (i = 0) => new Promise((res) => setTimeout(res, i * 100));
+const wait = (i) => new Promise((r) => setTimeout(r, i * 100));
+
+const getId = (() => {
+  let nextId = 0;
+  return () => ++nextId;
+})();
 
 // Load initial images
 const init = (async () => {
-  const idx = [...Array(COUNT).keys()];
-  const calls = idx.map((i) => wait(i).then(() => imageCreate(null, i)));
+  const idx = [...Array(12).keys()];
+  const calls = idx.map((i) => wait(i).then(imageCreate));
   await Promise.all(calls);
-
   return IMAGES.sort((a, b) => b.id - a.id);
 })();
 
